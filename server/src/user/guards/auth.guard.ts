6659057e63a6b0f298959e09ac1b2aba90847a38
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-02-09 13:07:30 +0100

    Prettier reformat

--- server/src/user/guards/auth.guard.ts
+++ server/src/user/guards/auth.guard.ts
@@ -3,9 +3,7 @@ import { ExpressRequestInterface } from '../../types/expressRequest.interface';
 
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    const request = context
-      .switchToHttp()
-      .getRequest<ExpressRequestInterface>();
+    const request = context.switchToHttp().getRequest<ExpressRequestInterface>();
 
     if (request.user) {
       return true;
