c4ee6cc4e780ee5ce6d946e251c9f8377a6f6ae2
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-02-19 15:46:29 +0100

    Save recipes to local storage and fetch from there

--- client/src/pages/Recipes.tsx
+++ client/src/pages/Recipes.tsx
@@ -2,17 +2,34 @@ import RecipeCard from "@/components/RecipeCard";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Recipe from "@/types/RecipeInterface";
+import { useEffect } from "react";
 
 const Recipes = () => {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["recipes"],
-    queryFn: async () => {
+  let cachedData = localStorage.getItem("recipes");
+  cachedData = JSON.parse(cachedData);
+
+  const fetchRecipeData = async () => {
+    try {
       const response = await axios.get("http://localhost:3000/recipes");
-      return response.data.recipes;
-    },
+      const recipes = response.data.recipes;
+
+      localStorage.setItem("recipes", JSON.stringify(recipes));
+
+      return recipes;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  };
+
+  const { isLoading, error } = useQuery({
+    queryKey: ["recipes"],
+    queryFn: fetchRecipeData,
   });
 
-  console.log(data);
+  useEffect(() => {
+    fetchRecipeData();
+  }, []);
 
   return (
     <div className="h-screen">
@@ -22,8 +39,12 @@ const Recipes = () => {
         <p>Error fetching data: {error.message}</p>
       ) : (
         <div className="p-10 flex gap-12">
-          {data &&
+          {/* {data &&
             data.map((recipe: Recipe) => (
+              <RecipeCard recipe={recipe} key={recipe.id} />
+            ))} */}
+          {cachedData &&
+            cachedData.map((recipe: Recipe) => (
               <RecipeCard recipe={recipe} key={recipe.id} />
             ))}
         </div>
