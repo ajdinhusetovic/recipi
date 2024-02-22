973b3538b0e4d73a7abe12098717078c8aedd866
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-02-22 17:03:55 +0100

    Add max and min length validation

--- server/src/user/dto/CreateUserDto.ts
+++ server/src/user/dto/CreateUserDto.ts
@@ -1,15 +1,16 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @MinLength(3, { message: 'Username should be at least 3 characters long. ' })
+  @MaxLength(30, { message: 'Username should not be longer than 20 characters. ' })
   @IsNotEmpty({ message: 'Username missing. ' })
   readonly username: string;
 
-  @IsNotEmpty({ message: 'Email missing. ' })
   @IsEmail({}, { message: 'Invalid email type. ' })
+  @IsNotEmpty({ message: 'Email missing. ' })
   readonly email: string;
 
+  @MinLength(8, { message: 'Password should be at least 8 characters long' })
   @IsNotEmpty({ message: 'Password missing.' })
   readonly password: string;
-
-  readonly bio?: string;
 }
