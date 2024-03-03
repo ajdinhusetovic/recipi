1d024b15f215d671c70a4a431682df07d1a45349
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-03-03 14:03:34 +0100

    Fix slug not updating

--- server/src/recipe/recipe.service.ts
+++ server/src/recipe/recipe.service.ts
@@ -184,11 +184,9 @@ export class RecipeService {
 
     existingRecipe.steps = savedSteps;
 
-    if (updateRecipeDto.name && updateRecipeDto.name !== existingRecipe.name) {
+    if (updateRecipeDto.name) {
       existingRecipe.slug =
         slugify(updateRecipeDto.name, { lower: true }) + '-' + ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
-    } else if (updateRecipeDto.name === existingRecipe.name && slug) {
-      existingRecipe.slug = slug;
     }
 
     const updatedRecipe = await this.recipeRepository.save(existingRecipe);
