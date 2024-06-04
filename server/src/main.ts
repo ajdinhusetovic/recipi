4f69a9cba5d033b98e1be555d04ee2070b96f256
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-06-04 13:41:36 +0200

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,8 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
     credentials: true,
   });
   await app.listen(3000);
