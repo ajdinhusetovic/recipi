f5865306e01d176ae02189ae192db5befdd2afd3
Ajdin Husetovic <ahusetovic1@gmail.com>
2024-06-04 13:35:58 +0200

    fix

--- client/src/pages/Recipes.tsx
+++ client/src/pages/Recipes.tsx
@@ -11,7 +11,7 @@ const Recipes = () => {
   const fetchRecipeData = async () => {
     try {
       const response = await axios.get(
-        "https://recipie-api.onrender.com/recipes",
+        "https://recipie-api.onrender.com/recipes"
       );
       const recipes = response.data.recipes;
 
